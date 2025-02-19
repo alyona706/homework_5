e3397879baadfd0e2196d76f1f96f4ee03ca4c83
alyona706 <almokina@gmail.com>
2025-02-19 10:48:53 +0200

    Initial commit

---
+++ 5.1/script.js
@@ -0,0 +1,7 @@
+let startNumber = 20;
+let endNumber = 30;
+let stepNumber = 0.5;
+
+for(startNumber; startNumber <= endNumber; startNumber += stepNumber) {
+    document.write(`<p>${startNumber}</p>`);
+}
